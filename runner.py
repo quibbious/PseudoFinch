c23a513ddf7e0f66dc07735297b092434e867b6a
quibb <108188557+quibbious@users.noreply.github.com>
2024-12-09 00:37:13 -0600

    Create runner.py

---
+++ runner.py
@@ -0,0 +1,22 @@
+from fwrite import Finch
+
+robot = Finch("Tommy",1)
+
+tests = {
+
+    "A": robot.beakLight(20, 20, 20),
+    "B": robot.setMove("F", 20, 100),
+    "C": robot.setTurn("R", 20, 100),
+    "D": robot.setMotors(100, 0),
+    "E": robot.setTail(3,100,50,20),
+    "F": robot.playNote(60, 1),
+    "G": robot.setDisplay([1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1]),
+    "H": robot.setPoint(3,3,1),
+    "I": robot.digiPrint("TEST")
+}
+
+
+for test in tests:
+    print(test)
+
+print("all tests passed")
